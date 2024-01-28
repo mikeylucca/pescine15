6dfa301a125b0c3782c760128acc714a9488fe97
Micael Lucca Soares Souza <misoares@c1r17s7.42lisboa.com>
2024-01-28 16:31:01 +0000

    up to ex02 done

---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: misoares <misoares@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/28 14:37:03 by misoares          #+#    #+#             */
+/*   Updated: 2024/01/28 15:11:56 by misoares         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+int	main()
+{
+	int n;
+	int *nbr8;
+	int **nbr7;
+	int ***nbr6;
+	int ****nbr5;
+	int *****nbr4;
+	int ******nbr3;
+	int *******nbr2;
+	int ********nbr1;
+	int *********nbr;
+
+	n = 42;
+
+	nbr8 = &n;
+	nbr7 = &nbr8;
+	nbr6 = &nbr7;
+	nbr5 = &nbr6;
+	nbr4 = &nbr5;
+	nbr3 = &nbr4;
+	nbr2 = &nbr3;
+	nbr1 = &nbr2;
+	nbr = &nbr1;
+
+	ft_ultimate_ft(nbr);
+	printf("%p", nbr);
+	return (0);
+}
---
+++ C01/ex02/ft_swap.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: misoares <misoares@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/28 15:15:03 by misoares          #+#    #+#             */
+/*   Updated: 2024/01/28 15:57:12 by misoares         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_swap(int *a, int *b)
+{
+	int temp; // temp variable to store the first value temporarily
+
+	temp = *a; // temp value is swapped to *a 
+	*a = *b; // *a value is swapped to *b 
+	*b = temp; // *b value is swapped to temp 
+}
+
+int main()
+{
+	int x;
+	int y; // declares 2 int variables
+
+	x = 5;
+	y = 2; // declare the value of the int variables
+
+	ft_swap(&x, &y); // swaps position of values
+	printf("X: %d\n", x); // prints swapped values
+	printf("Y: %d\n", y);
+
+	return(0);
+}
