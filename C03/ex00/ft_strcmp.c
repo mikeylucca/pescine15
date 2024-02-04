9ebd5e872825278690b95aef38cfeea66097f201
MikeLaptopLinux <mike.souza2k19@gmail.com>
2024-02-04 21:21:49 +0000

    norminette errors

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -13,10 +13,11 @@
 int		ft_strcmp(char *s1, char *s2)
 {
 	// Loop until both strings are not terminated ('\0') and the characters are equal
-    while (*s1 && *s2 && *s1 == *s2) {
-        // Move to the next character in both strings
-        s1++;
-        s2++;
-    }
+	while (*s1 && *s2 && *s1 == *s2) 
+	{
+		// Move to the next character in both strings
+		s1++;
+		s2++;
+	}
 	return (*s1) - (*s2);
 }
